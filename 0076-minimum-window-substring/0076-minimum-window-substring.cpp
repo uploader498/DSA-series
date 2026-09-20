fac5c12cf3fb96091097123fa5b30db1d9f44eff
uploader498 <raikwarnikhil519@gmail.com>
2026-09-20 20:30:29 +0530

    Time: 7 ms (60.67%), Space: 11.5 MB (64.84%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int>m;
+        for(int i=0;i<t.size();i++){
+            m[t[i]]++;
+        }
+        int total=t.size(),start=0,end=0,ans=INT_MAX,index=-1;
+        while(end<s.size()){
+            m[s[end]]--;
+            if(m[s[end]]>=0){
+                total--;
+            }
+            while(!total && start<=end){
+                if(ans > (end-start)+1){
+                    ans = (end-start)+1;
+                    index=start;
+                }
+                m[s[start]]++;
+                if(m[s[start]]>0){
+                    total++;
+                }
+                start++;
+            }
+            end++;
+        }
+        if(index==-1)
+        return "";
+
+        string str = "";
+        for(int i=index;i<index+ans;i++){
+            str += s[i];
+        }
+        return str;
+    }
+};
